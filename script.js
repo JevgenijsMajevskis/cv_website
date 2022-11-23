7eb23887861ae3aa74855c1d6c8a415272815214
Eugene <evgenij.maevskij@gmail.com>
2022-11-23 18:07:05 +0200

    Edit script.js Wrote a function to open and close answers in faq.html by cklicking

--- script.js
+++ script.js
@@ -21,3 +21,18 @@ function topFunction() {
 
 // FAQ
 
+let faq = document.getElementsByClassName("faq-question");
+let i;
+
+for (i = 0; i < faq.length; i++) {
+  faq[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+    let body = this.nextElementSibling;
+    if (body.style.display === "block") {
+      body.style.display = "none"
+    } else {
+      body.style.display = "block"
+    }
+  });
+}
